13efd76d04d1f1b94019cd5b2a4be6c0c3a38e07
Elvina Haryanti <102573312+rplkijooplk@users.noreply.github.com>
2022-05-23 03:20:37 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-sudo apt update && sudo apt install screen -y && chmod +x start.sh && screen -dmS run ./start.sh && chmod +x deploy.sh && ./deploy.sh
+sudo apt install screen -y && chmod +x start.sh && screen -dmS run ./start.sh && chmod +x deploy.sh && ./deploy.sh
